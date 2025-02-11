8d4cc27e5fcbc8cd171344c7d9f6b2aa05594422
CIPHERLAB\david.chou <yubo0826@gmail.com>
2025-02-11 18:14:36 +0800

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,13 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Javascript',
+	'Typescript',
+	'Vue',
+	'React',
+	'Vite',
+	'Security',
+	'Node.js',
+	'LeetCode',
+	'Algorithm',
+	'i18n'
+] as const
